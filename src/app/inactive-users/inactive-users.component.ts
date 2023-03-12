67f961e28cd45c0be237562321ad101dd5a09f0f
Jakub Jóźwiak <jozwiak.ja@icloud.com>
2023-03-12 17:11:24 +0100

    Create full valid form

---
+++ src/app/inactive-users/inactive-users.component.ts
@@ -0,0 +1,20 @@
+import { UsersService } from "./../users.service";
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-inactive-users",
+  templateUrl: "./inactive-users.component.html",
+  styleUrls: ["./inactive-users.component.css"],
+})
+export class InactiveUsersComponent implements OnInit {
+  users: string[] = [];
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit(): void {
+    this.users = this.usersService.activeUsers;
+  }
+
+  onSetToActive(id: number) {
+    this.usersService.addToActive(id);
+  }
+}
